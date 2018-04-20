11df66497a9c1427381f91db7e9d74f802f176d7
Zhuinden <zhuinden@gmail.com>
2018-04-20 23:17:06 +0200

    Initial commit

---
+++ app/src/main/java/com/zhuinden/casterio_simple_stack_examples/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zhuinden.casterio_simple_stack_examples
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
