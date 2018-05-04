e4dddc8eefbe628e59c3edfaf1ee884b91ef9c56
Zhuinden <zhuinden@gmail.com>
2018-05-04 03:47:35 +0200

    Clean things up a bit

--- app/src/main/java/com/zhuinden/casterio_simple_stack_examples/MainActivity.kt
+++ app/src/main/java/com/zhuinden/casterio_simple_stack_examples/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.zhuinden.casterio_simple_stack_examples
 
 import android.os.Bundle
-import android.os.Parcelable
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.zhuinden.simplestack.*
-import kotlinx.android.parcel.Parcelize
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.sdk15.listeners.onClick
 
+@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity(), StateChanger {
-    @Parcelize
-    data class Screen(val screen: String) : Parcelable
-
     private lateinit var backstackDelegate: BackstackDelegate
     private lateinit var backstack: Backstack
 
@@ -20,7 +13,7 @@ class MainActivity : AppCompatActivity(), StateChanger {
         backstackDelegate = BackstackDelegate()
         backstackDelegate.onCreate(savedInstanceState,
             lastCustomNonConfigurationInstance,
-            History.single(Screen("FIRST")))
+            History.single(TODO()))
         backstackDelegate.registerForLifecycleCallbacks(this)
 
         backstack = backstackDelegate.backstack
@@ -45,33 +38,10 @@ class MainActivity : AppCompatActivity(), StateChanger {
     override fun handleStateChange(stateChange: StateChange, completionCallback: StateChanger.Callback) {
         // this is where we handle navigation events.
         // when the navigation event change is handled, the completion callback must be called.
-        Log.i("MainActivity",
-            "\nNavigation States:\n" +
-                "Previous: [${stateChange.getPreviousState<Screen>().asString()}\n" +
-                "New: ${stateChange.getNewState<Screen>().asString()}]\n")
         completionCallback.stateChangeComplete()
     }
 
     // handle button clicks
     private fun setupClicks() {
-        goToScreenB.onClick {
-            backstack.goTo(Screen("B"))
-        }
-
-        goToScreenC.onClick {
-            backstack.goTo(Screen("C"))
-        }
-
-        setHistoryD.onClick {
-            backstack.setHistory(History.single(Screen("D")), StateChange.REPLACE)
-        }
-
-        goUpToC.onClick {
-            backstack.goUp(Screen("C"))
-        }
-
-        goUpChainToAB.onClick {
-            backstack.goUpChain(History.of(Screen("A"), Screen("B")))
-        }
     }
 }
